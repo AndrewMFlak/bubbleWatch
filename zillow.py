18fc6398c3b00e61ec24b68a6e4c049c6f929a89
AFlak <AndrewMFlak@gmail.com>
2020-01-05 20:41:46 -0500

    updated push

--- zillow.py
+++ zillow.py
@@ -4,5 +4,8 @@ from os.path import join, dirname
 import dotenv
 from dotenv import load_dotenv
 dotenv_path=join(dirname(__file__),'.env')
+load_dotenv(dotenv_path)
 zipCode = os.getenv('placeholder')
+zillowAPIkey = os.getenv('zillowAPIkey')
 print(zipCode)
+print(zillowAPIkey)
